d48f1b5027da68c3dbde7e0c864015617a87b097
Captain <mads130f@edu.easj.dk>
2018-09-12 15:13:30 +0200

    kermit

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,9 +1,37 @@
+import java.io.DataOutputStream;
 import java.net.*;
 
 public class MyClient {
 
     public static void main(String[] args) throws Exception {
-        Socket s = new Socket("127.0.0.1", 2020);
+        //Socket s = new Socket("127.0.0.1", 2020);
+
+
+        Socket socket = new Socket("127.0.0.1",2020); // Create and connect the socket
         System.out.println("Connection Established");
+
+        DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+
+// Send first message
+        dOut.writeByte(1);
+        dOut.writeUTF("This is the first type of message.");
+        dOut.flush(); // Send off the data
+
+// Send the second message
+        dOut.writeByte(2);
+        dOut.writeUTF("This is the second type of message.");
+        dOut.flush(); // Send off the data
+
+// Send the third message
+        dOut.writeByte(3);
+        dOut.writeUTF("This is the third type of message (Part 1).");
+        dOut.writeUTF("This is the third type of message (Part 2).");
+        dOut.flush(); // Send off the data
+
+// Send the exit message
+        dOut.writeByte(-1);
+        dOut.flush();
+
+        dOut.close();
     }
 }
--- src/MyServer.java
+++ src/MyServer.java
@@ -1,12 +1,45 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
 import java.net.*;
 
 public class MyServer {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+
         ServerSocket s = new ServerSocket(2020);
         System.out.println("Server waiting for connection....");
+
         Socket so = s.accept();
         System.out.println("Connection Established");
+
+        System.out.println("message send!!");
+
+        System.out.println("Stops here");
+        DataInputStream dIn = new DataInputStream(so.getInputStream());
+
+        boolean done = false;
+        while(!done) {
+            byte messageType = dIn.readByte();
+
+            switch(messageType)
+            {
+                case 1: // Type A
+                    System.out.println("Message A: " + dIn.readUTF());
+                    break;
+                case 2: // Type B
+                    System.out.println("Message B: " + dIn.readUTF());
+                    break;
+                case 3: // Type C
+                    System.out.println("Message C [1]: " + dIn.readUTF());
+                    System.out.println("Message C [2]: " + dIn.readUTF());
+                    break;
+                default:
+                    done = true;
+            }
+        }
+
+        dIn.close();
     }
 }
