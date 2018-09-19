c7992f289086957a8fd34943cd822a75c5ad693e
wexr <wexr.kk@gmail.com>
2018-09-19 15:37:09 +0200

    De forskellige tjeks af win conditions.

--- src/TicTacToeBackend/Logik.java
+++ src/TicTacToeBackend/Logik.java
@@ -6,6 +6,7 @@ public class Logik {
     TicPrint ticprint = new TicPrint();
     private char[] bræt = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}; //Spille brættet, lavet af et single array.
     private boolean xTurn; //Sandt hvis det er X's tur, false hvis det er O's tur.
+    private boolean winner = false; // Antager som udgangspunkt at der ingen vinder er.
 
 
 
@@ -33,7 +34,100 @@ public class Logik {
 
 
     public String checkIfOver(){
-    if(){
+        char checkX = 'X';
+        char checkO = 'O';
+        int sum = 0;
+
+    // Denne tjekker 1 2 3. Er lavet.
+        for(int x = 0; x < 2; x++){
+        if(bræt[x]== checkX) {
+            sum++;
+        }
+    }
+
+        if (sum == 3){
+            winner = true;
+        }
+
+    // Denne skal tjekke 4 5 6
+        sum = 0;
+    for(int x = 0; x < 2; x++){
+        if(bræt[x]== checkX) {
+            sum++;
+        }
+    }
+        if (sum == 3){
+            winner = true;
+        }
+
+        // Denne skal tjekke 7 8 9
+        sum = 0;
+        for(int x = 0; x < 2; x++){
+            if(bræt[x]== checkX) {
+                sum++;
+            }
+        }
+        if (sum == 3){
+            winner = true;
+        }
+
+        // Denne skal tjekke 1 4 7
+        sum = 0;
+        for(int x = 0; x < 2; x++){
+            if(bræt[x]== checkX) {
+                sum++;
+            }
+        }
+        if (sum == 3){
+            winner = true;
+        }
+        // Denne skal tjekke 2 5 8
+        sum = 0;
+        for(int x = 0; x < 2; x++){
+            if(bræt[x]== checkX) {
+                sum++;
+            }
+        }
+        if (sum == 3){
+            winner = true;
+        }
+        // Denne skal tjekke 3 6 9
+        sum = 0;
+        for(int x = 0; x < 2; x++){
+            if(bræt[x]== checkX) {
+                sum++;
+            }
+        }
+        if (sum == 3){
+            winner = true;
+        }
+        // Denne skal tjekke 1 5 9
+        sum = 0;
+        for(int x = 0; x < 2; x++){
+            if(bræt[x]== checkX) {
+                sum++;
+            }
+        }
+        if (sum == 3){
+            winner = true;
+        }
+        // Denne skal tjekke 3 5 8
+        sum = 0;
+        for(int x = 0; x < 2; x++){
+            if(bræt[x]== checkX) {
+                sum++;
+            }
+        }
+        if (sum == 3){
+            winner = true;
+        }
+
+
+
+
+
+
+    else if(){
 
     }
 
@@ -50,3 +144,6 @@ public class Logik {
 
 
 }
+
+
+
--- src/TicTacToeBackend/TicPrint.java
+++ src/TicTacToeBackend/TicPrint.java
@@ -5,14 +5,14 @@ public class TicPrint {
 
 
 
-    public void printboard(char board[]){
+    public String printboard(char[] board){
         System.out.println("ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ");
         String newboard = "      |"+board[0]+"|"+board[1]+"|"+board[2]+"|\n"+"      |"+board[3]+"|"+board[4]+"|"+board[5]+"|\n"+"      |"+board[6]+"|"+board[7]+"|"+board[8]+"|\n" ;
 
         System.out.println(newboard);
 
 
-
+        return newboard;
     }
 
 }
