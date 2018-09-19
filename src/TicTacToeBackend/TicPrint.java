31ef822a37bcb414ea0db3835214ea516ccc5960
mathiasbruun <math686f@edu.easj.dk>
2018-09-19 13:29:15 +0200

    endnu sejere board

--- src/TicTacToeBackend/TicPrint.java
+++ src/TicTacToeBackend/TicPrint.java
@@ -6,9 +6,11 @@ public class TicPrint {
 
 
     public void printboard(char board[]){
-        System.out.println("| | | |");
-        System.out.println("| | | |");
-        System.out.println("| | | |");
+        System.out.println("ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ");
+        String newboard = "      |"+board[0]+"|"+board[1]+"|"+board[2]+"|\n"+"      |"+board[3]+"|"+board[4]+"|"+board[5]+"|\n"+"      |"+board[6]+"|"+board[7]+"|"+board[8]+"|\n" ;
+
+        System.out.println(newboard);
+
 
 
     }
@@ -16,7 +18,7 @@ public class TicPrint {
     public static void main(String[] args) {
         char board[] = new char[9];
         board[0] = 'x';
-        board[1] = '-';
+        board[1] = 'o';
         board[2] = ' ';
         board[3]= ' ';
         board[4] = ' ';
