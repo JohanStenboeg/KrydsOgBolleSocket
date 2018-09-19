025b16cd73a0aaa83191cf17d22a083381a9465b
mathiasbruun <math686f@edu.easj.dk>
2018-09-19 13:34:38 +0200

    Sejt ascii board x4

--- src/TicTacToeBackend/TicPrint.java
+++ src/TicTacToeBackend/TicPrint.java
@@ -3,31 +3,11 @@ package TicTacToeBackend;
 public class TicPrint {
 
 
+    // metode som returnerer det pæne ascii board samt overskrift.
+    public String printboard(char board[]) {
+        String newboard = "ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ\n" + "      |" + board[0] + "|" + board[1] + "|" + board[2] + "|\n" + "      |" + board[3] + "|" + board[4] + "|" + board[5] + "|\n" + "      |" + board[6] + "|" + board[7] + "|" + board[8] + "|\n";
 
 
-    public void printboard(char board[]){
-        System.out.println("ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ");
-        String newboard = "      |"+board[0]+"|"+board[1]+"|"+board[2]+"|\n"+"      |"+board[3]+"|"+board[4]+"|"+board[5]+"|\n"+"      |"+board[6]+"|"+board[7]+"|"+board[8]+"|\n" ;
-
-        System.out.println(newboard);
-
-
-
-    }
-
-    public static void main(String[] args) {
-        char board[] = new char[9];
-        board[0] = 'x';
-        board[1] = 'o';
-        board[2] = ' ';
-        board[3]= ' ';
-        board[4] = ' ';
-        board[5] = ' ';
-        board[6] = ' ';
-        board[7] = ' ';
-        board[8] = ' ';
-
-        TicPrint ticPrint = new TicPrint();
-        ticPrint.printboard(board);
+        return newboard;
     }
 }
