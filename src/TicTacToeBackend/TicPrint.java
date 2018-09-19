1dc9386acb704778038faa41e285be8681092812
mathiasbruun <math686f@edu.easj.dk>
2018-09-19 15:33:38 +0200

    Vi går tilbage til 1 dim

--- src/TicTacToeBackend/TicPrint.java
+++ src/TicTacToeBackend/TicPrint.java
@@ -5,14 +5,19 @@ public class TicPrint {
 
 
 
-    public void printboard(char board[]){
-        System.out.println("ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ");
-        String newboard = "      |"+board[0]+"|"+board[1]+"|"+board[2]+"|\n"+"      |"+board[3]+"|"+board[4]+"|"+board[5]+"|\n"+"      |"+board[6]+"|"+board[7]+"|"+board[8]+"|\n" ;
+    public String printboard(char board[]){
 
-        System.out.println(newboard);
+        String newboard = "ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ\n      |"+board[0]+"|"+board[1]+"|"+board[2]+"|\n"+"      |"+board[3]+"|"+board[4]+"|"+board[5]+"|\n"+"      |"+board[6]+"|"+board[7]+"|"+board[8]+"|\n" ;
+        //System.out.println("ⓣⓘⓒ  ⓣⓐⓒ  ⓣⓞⓔ");
+        //System.out.println(newboard);
 
 
+        return newboard;
+    }
 
+    public static void main(String[] args) {
+        TicPrint ticPrint = new TicPrint();
+        System.out.println(ticPrint.printboard(new char[9]));
     }
 
 }
